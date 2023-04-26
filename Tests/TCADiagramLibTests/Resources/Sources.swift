ef6a33eb5b886ec7f9c4232a049b8c7d9f8a58e0
Jaeyoung Yoon <lastmore9@gmail.com>
2023-04-26 14:46:19 +0900

    🤡 Add reducerProtocol samples

--- Tests/TCADiagramLibTests/Resources/Sources.swift
+++ Tests/TCADiagramLibTests/Resources/Sources.swift
@@ -74,3 +74,66 @@ let sources: [String] = [
   }
   """,
 ]
+
+let reducerProtocolSampleSource: [String] = [
+  """
+  public struct SelfLessonDetail: ReducerProtocol {
+    @Dependency(\\.environmentSelfLessonDetail) private var environment
+
+    public init() {}
+
+    public var body: some ReducerProtocol<State, Action> {
+      BindingReducer()
+      Scope(state: \\State.payment, action: /Action.payment) {
+        Payment()
+      }
+
+      Scope(state: \\.subState, action: .self) {
+        Scope(
+          state: /State.SubState.promotionWeb,
+          action: /Action.promotionWeb
+        ) {
+          DoubleScopeChild()
+        }
+      }
+
+      Reduce { state, action in
+        switch action {
+          case default:
+            return .none
+        }
+      }
+      .ifLet(\\.filter, action: /Action.filter) {
+        SelfLessonDetailFilter()
+      }
+      .ifLet(\\.selection, action: /Action.web) {
+        SantaWeb()
+      }
+      .ifLet(\\SelfLessonDetail.State.selection, action: /SelfLessonDetail.Action.webView) {
+        EmptyReducer()
+          .ifLet(\\Identified.value, action: .self) {
+            DoubleIfLetChild()
+          }
+      }
+    }
+  }
+  """,
+  """
+  extension SelfLessonDetail {
+    public enum Action: Equatable {
+    }
+  }
+  extension Payment {
+    public enum Action: Equatable {
+    }
+  }
+  extension SantaWeb {
+    public enum Action: Equatable {
+    }
+  }
+  extension SelfLessonDetailFilter {
+    public enum Action: Equatable {
+    }
+  }
+  """
+]
